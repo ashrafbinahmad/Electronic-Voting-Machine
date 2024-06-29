0bad85ff734c92f44a9aaecf5ffa68efa71d8806
ashrafp216 <ashrafp216@gmail.com>
2024-06-29 18:45:26 +0530

    added for madrassa for shahbaz

--- utils/Consts.js
+++ utils/Consts.js
@@ -1,55 +1,90 @@
 const candidatesUnderCategories = [
     {
-        category: 'President',
+        category: 'BOY LEADER',
         candidates: [
             {
-                name: 'Unais Pallathadukka',
-                image: 'candidate-images/presi1.jpg',
+                name: 'Muhammed Nihal',
+                image: 'candidate-images/cand 3.jpg',
                 description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl quis ultricies ultricies, nunc nisl',
                 voted: false
             },
             {
-                name: 'Khader Uppinangady',
-                image: 'candidate-images/presi2.jpg',
+                name: 'Muhammad B.B',
+                image: 'candidate-images/cand 4.jpg',
                 description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl quis ultricies ultricies, nunc nisl',
                 voted: false
-            }
+            },
         ]
     },
     {
-        category: 'Secretary',
+        category: 'GIRL LEADER',
+
         candidates: [
             {
-                name: 'Afsal Oduvar',
-                image: 'candidate-images/secr1.jpg',
+                name: 'Seyyeadath Zuha Fathima',
+                image: 'candidate-images/cand 1.jpg',
                 description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl quis ultricies ultricies, nunc nisl',
                 voted: false
             },
             {
-                name: 'Shameel Puttur',
-                image: 'candidate-images/secr2.jpg',
-                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl quis ultricies ultricies, nunc nisl',
-                voted: false
-            }
-        ]
-    },
-    {
-        category: 'Treasurer',
-        candidates: [
-            {
-                name: 'Shahan Santhoshnagar',
-                image: 'candidate-images/treas1.jpg',
+                name: 'Khadeejath Safreena',
+                image: 'candidate-images/cand 2.jpg',
                 description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl quis ultricies ultricies, nunc nisl',
                 voted: false
             },
-            {
-                name: 'Mazin Mogral',
-                image: 'candidate-images/treas2.jpg',
-                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl quis ultricies ultricies, nunc nisl',
-                voted: false
-            }
         ]
     },
+    // {
+    //     category: 'President',
+    //     candidates: [
+    //         {
+    //             name: 'Unais Pallathadukka',
+    //             image: 'candidate-images/presi1.jpg',
+    //             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl quis ultricies ultricies, nunc nisl',
+    //             voted: false
+    //         },
+    //         {
+    //             name: 'Khader Uppinangady',
+    //             image: 'candidate-images/presi2.jpg',
+    //             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl quis ultricies ultricies, nunc nisl',
+    //             voted: false
+    //         }
+    //     ]
+    // },
+    // {
+    //     category: 'Secretary',
+    //     candidates: [
+    //         {
+    //             name: 'Afsal Oduvar',
+    //             image: 'candidate-images/secr1.jpg',
+    //             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl quis ultricies ultricies, nunc nisl',
+    //             voted: false
+    //         },
+    //         {
+    //             name: 'Shameel Puttur',
+    //             image: 'candidate-images/secr2.jpg',
+    //             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl quis ultricies ultricies, nunc nisl',
+    //             voted: false
+    //         }
+    //     ]
+    // },
+    // {
+    //     category: 'Treasurer',
+    //     candidates: [
+    //         {
+    //             name: 'Shahan Santhoshnagar',
+    //             image: 'candidate-images/treas1.jpg',
+    //             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl quis ultricies ultricies, nunc nisl',
+    //             voted: false
+    //         },
+    //         {
+    //             name: 'Mazin Mogral',
+    //             image: 'candidate-images/treas2.jpg',
+    //             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl quis ultricies ultricies, nunc nisl',
+    //             voted: false
+    //         }
+    //     ]
+    // },
 ]
 
 export default candidatesUnderCategories;
